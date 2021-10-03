064d16872259655698ff15bc00a481b805dc0dd9
Grant Pearce <grant.pearce@cazoo.co.uk>
2021-10-03 10:34:16 +0100

    add mui start with app bar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,6 +1,9 @@
 import React, { useContext } from 'react'
 import { Link } from 'react-router-dom';
 import { SearchContext } from '../../contexts/SearchContext';
+import AppBar from '@mui/material/AppBar';
+import { Toolbar, Typography } from '@mui/material';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
 
 
 const Navbar = () => {
@@ -9,14 +12,24 @@ const Navbar = () => {
 
     return (
         <div>
+            <AppBar position="static" sx={{ bgcolor: 'primary.main' }}>
+                <Toolbar>
+                    <Link to={`/`} >
+                        <Typography variant="h6" component="div"  sx={{ color: 'common.white' }}>
+                            <LiveTvIcon  sx={{ mr: 1 }} />
+                            TV Series
+                        </Typography>
+                    </Link>
+                </Toolbar>
+            </AppBar>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
 
-                <Link to={`/`} className="navbar-brand">
+                {/* <Link to={`/`} className="navbar-brand">
                     <span className="fas fa-tv" />
                     <span className="h4">
                         &nbsp; TV Series
                     </span>
-                </Link>
+                </Link> */}
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -24,9 +37,9 @@ const Navbar = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item active">
-                            <Link to={`/`} className="nav-link">
+                            {/* <Link to={`/`} className="nav-link">
                                 Home <span className="sr-only">(current)</span>
-                            </Link>
+                            </Link> */}
                         </li>
                     </ul>
                     <input className="form-control search"  type="text" placeholder="Search..." onChange={ onSeriesInputChange } onKeyDown={ onSeriesInputKeyDown} />
